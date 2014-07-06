2a4702ce5dbf7fbec6ab02ed7e4c65e28ff4ad36
Brian Pollack <brian.pollack@cern.ch>
2014-07-06 11:58:52 -0500

    cleanup MC_GEN

--- drivers/driverMacroPOWHEG_RD1.py
+++ drivers/driverMacroPOWHEG_RD1.py
@@ -55,6 +55,7 @@ elif int(step) == 1:
 elif int(step) == 2:
   os.system('cmsDriver.py STEP2_PYTHIA8_POWHEG_RD1_H_Zg_8TeV \
       --step RAW2DIGI,L1Reco,RECO,VALIDATION:validation_prod,DQM:DQMOfflinePOGMC \
+      --runsScenarioForMC Run2012_AB_C_D_oneRunPerEra \
       --conditions START53_V7N::All \
       --pileup fromDB\
       --pileup_input dbs:/RelValMinBias/CMSSW_5_2_1-START52_V4-v1/GEN-SIM \
