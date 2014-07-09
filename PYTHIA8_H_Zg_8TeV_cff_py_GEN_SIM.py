7ac0e48d6d67254b99567f75641e832911087bb7
Brian Pollack <brian.pollack@cern.ch>
2014-07-09 14:22:22 -0500

    need to update release

---
+++ PYTHIA8_H_Zg_8TeV_cff_py_GEN_SIM.py
@@ -0,0 +1,100 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.28 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: PYTHIA8_H_Zg_8TeV_cff.py --step GEN,SIM --conditions START53_V7N::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.28 $'),
+    annotation = cms.untracked.string('PYTHIA8_H_Zg_8TeV_cff.py nevts:1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('PYTHIA8_H_Zg_8TeV_cff_py_GEN_SIM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7N::All', '')
+
+process.generator = cms.EDFilter("Pythia8175GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(8000),
+    maxEventsToPrint = cms.untracked.int32(1),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring('Tune:pp = 5', 
+            'PDF:pSet = 7', 
+            'HiggsSM:gg2H = on', 
+            '25:m0 = 200.0', 
+            '25:onMode = off', 
+            '25:onIfMatch = 23 22', 
+            '23:mMin = 50.0', 
+            '23:onMode = off', 
+            '23:onIfAny = 11 13 15'),
+        parameterSets = cms.vstring('processParameters')
+    )
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
--- drivers/driverMacroLO_RD1.py
+++ drivers/driverMacroLO_RD1.py
@@ -31,38 +31,37 @@ import os
 
 step = raw_input('enter step 0, 1, or 2: ')
 if int(step) == 0:
-  os.system('cmsDriver.py PYTHIA8_POWHEG_RD1_H_Zg_8TeV_cff.py \
+  os.system('cmsDriver.py PYTHIA8_H_Zg_8TeV_cff.py \
       --step GEN,SIM \
       --conditions START53_V7N::All \
       --pileup NoPileUp \
       --datamix NODATAMIXER \
       --eventcontent RAWSIM \
       --datatier GEN-SIM \
-      --filein=file:/eos/uscms/store/user/bpollack/lhe/h_ggH_WW_ZGamma_125_15.lhe \
       --no_exec')
 elif int(step) == 1:
-  os.system('cmsDriver.py REDIGI_PYTHIA8_POWHEG_RD1_H_Zg_8TeV \
+  os.system('cmsDriver.py REDIGI \
       --step DIGI,L1,DIGI2RAW,HLT:7E33v2 \
       --runsScenarioForMC Run2012_AB_C_D_oneRunPerEra \
       --conditions START53_V7N::All \
-      --pileup fromDB\
       --pileup_input dbs:/RelValMinBias/CMSSW_5_2_1-START52_V4-v1/GEN-SIM \
+      --pileup fromDB\
       --datamix NODATAMIXER \
       --eventcontent RAWSIM \
       --datatier GEN-SIM-RAW \
-      --filein file:PYTHIA8_POWHEG_RD1_H_Zg_8TeV_cff_py_GEN_SIM.root \
+      --filein file:PYTHIA8_H_Zg_8TeV_cff_py_GEN_SIM.root \
       --no_exec')
 elif int(step) == 2:
-  os.system('cmsDriver.py STEP2_PYTHIA8_POWHEG_RD1_H_Zg_8TeV \
+  os.system('cmsDriver.py STEP2 \
       --step RAW2DIGI,L1Reco,RECO,VALIDATION:validation_prod,DQM:DQMOfflinePOGMC \
       --runsScenarioForMC Run2012_AB_C_D_oneRunPerEra \
       --conditions START53_V7N::All \
-      --pileup fromDB\
       --pileup_input dbs:/RelValMinBias/CMSSW_5_2_1-START52_V4-v1/GEN-SIM \
+      --pileup fromDB\
       --datamix NODATAMIXER \
       --eventcontent AODSIM,DQM \
       --datatier AODSIM,DQM \
-      --filein file:REDIGI_PYTHIA8_RD1_H_Zg_8TeV_DIGI_L1_DIGI2RAW_HLT_PU.root \
+      --filein file:REDIGI_DIGI_L1_DIGI2RAW_HLT_PU.root \
       --no_exec')
 else:
   print 'how did you fuck this up?'
